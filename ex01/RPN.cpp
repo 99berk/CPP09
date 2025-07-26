0e6d17c7cc216793c0513fe674927c54ffa6ccb7
99berk <98461391+99berk@users.noreply.github.com>
2025-07-27 01:24:59 +0300

    Refactor RPN calculation logic for improved readability and error handling

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -18,7 +18,13 @@ RPN::~RPN() {}
 
 bool RPN::isOperator(const std::string& token)
 {
-    return (token == "+" || token == "-" || token == "*" || token == "/");
+    const char* operators[] = { "+", "-", "*", "/" };
+    for (size_t i = 0; i < 4; ++i)
+    {
+        if (token == operators[i])
+            return true;
+    }
+    return false;
 }
 
 bool RPN::isNumber(const std::string& token)
@@ -29,7 +35,8 @@ bool RPN::isNumber(const std::string& token)
     size_t start = 0;
     if (token[0] == '-' || token[0] == '+')
     {
-        if (token.length() == 1) return false;
+        if (token.length() == 1) 
+            return false;
         start = 1;
     }
     
@@ -62,35 +69,48 @@ int RPN::performOperation(int a, int b, char op)
 
 int RPN::calculate(const std::string& expression)
 {
-    std::istringstream iss(expression);
-    std::string token;
-    
-    while (iss >> token)
+    size_t pos = 0;
+    while (pos < expression.length())
     {
-        if (isNumber(token))
+        while (pos < expression.length() && expression[pos] == ' ')
+            ++pos;
+
+        size_t token_end = pos;
+        while (token_end < expression.length() && expression[token_end] != ' ')
+            ++token_end;
+
+        std::string token = expression.substr(pos, token_end - pos);
+
+        if (!token.empty())
         {
-            int num = std::atoi(token.c_str());
-            if (num >= 10 || num < 0)
-                throw std::runtime_error("Numbers must be less than 10 and non-negative");
-            _stack.push(num);
+            if (isNumber(token))
+            {
+                int num = std::atoi(token.c_str());
+                if (num >= 10 || num < 0)
+                    throw std::runtime_error("Numbers must be less than 10 and non-negative");
+                _stack.push(num);
+            }
+            else if (isOperator(token))
+            {
+                if (_stack.size() < 2)
+                    throw std::runtime_error("Insufficient operands");
+
+                int b = _stack.top();
+                _stack.pop();
+                int a = _stack.top();
+                _stack.pop();
+
+                int result = performOperation(a, b, token[0]);
+                _stack.push(result);
+            }
+            else
+                throw std::runtime_error("Invalid token");
         }
-        else if (isOperator(token))
-        {
-            if (_stack.size() < 2)
-                throw std::runtime_error("Insufficient operands");
-            
-            int b = _stack.top(); _stack.pop();
-            int a = _stack.top(); _stack.pop();
-            
-            int result = performOperation(a, b, token[0]);
-            _stack.push(result);
-        }
-        else
-            throw std::runtime_error("Invalid token");
+        pos = token_end + 1;
     }
-    
+
     if (_stack.size() != 1)
         throw std::runtime_error("Invalid expression");
-    
+
     return _stack.top();
 }
